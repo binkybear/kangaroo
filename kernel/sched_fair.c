4e5a475496fe68a5a3468c752c0e668bdae00e21
Metallice <BryanUofD@gmail.com>
2012-11-06 15:49:59 -0500

    sched_fair.c: tweak cfs parameters
    Conflicts:
    	kernel/sched_fair.c

--- kernel/sched_fair.c
+++ kernel/sched_fair.c
@@ -25,6 +25,7 @@
 #include <linux/cpumask.h>
 
 /*
+<<<<<<< HEAD
 * Targeted preemption latency for CPU-bound tasks:
 * (default: 6ms * (1 + ilog(ncpus)), units: nanoseconds)
 *
@@ -36,6 +37,19 @@
 * (to see the precise effective timeslice length of your workload,
 * run vmstat and monitor the context-switches (cs) field)
 */
+=======
+ * Targeted preemption latency for CPU-bound tasks:
+ * (default: 6ms * (1 + ilog(ncpus)), units: nanoseconds)
+ *
+ * NOTE: this latency value is not the same as the concept of
+ * 'timeslice length' - timeslices in CFS are of variable length
+ * and have no persistent notion like in traditional, time-slice
+ * based scheduling concepts.
+ *
+ * (to see the precise effective timeslice length of your workload,
+ *  run vmstat and monitor the context-switches (cs) field)
+ */
+>>>>>>> f59e715... sched_fair.c: tweak cfs parameters
 unsigned int sysctl_sched_latency = 4000000ULL;
 unsigned int normalized_sysctl_sched_latency = 4000000ULL;
 
